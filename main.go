6b959622f3a6f85973426cf2ab8d44fb789753ef
Ramiro Luna <92827657+ramiroluna2@users.noreply.github.com>
2025-04-14 23:40:21 -0300

    Ahora pide los votos por teclado

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"math/rand"
+	"fmt"
 )
 
 func main() {
 
 	votos := []int{}
 	for i := 0; i < 10; i++ {
-		voto := rand.Intn(5) + 1
+		var voto int
+		println("Introduce el voto (1-5):")
+		_, err := fmt.Scan(&voto)
+		if err != nil || voto < 1 || voto > 5 {
+			println("Voto no válido, introduce un número entre 1 y 5")
+			i--
+			continue
+		}
 		votos = append(votos, voto)
 	}
+
 	votosContados := make(map[int]int)
 	for _, voto := range votos {
 		votosContados[voto]++
